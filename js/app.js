806d4d087be535c56fad88c4284e4fb3fa7520fb
arkhan66648 <rmadskhanfirst4323@gmail.com>
2026-07-08 21:05:47 +0600

    fix: move Histats _Hasync to global scope so tracking data reaches dashboard

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function(){var _Hasync=_Hasync||[];_Hasync.push(['Histats.start','1,5037469,4,0,0,0,00010000']);_Hasync.push(['Histats.fasi','1']);_Hasync.push(['Histats.track_hits','']);var hs=document.createElement('script');hs.type='text/javascript';hs.async=true;hs.src=('//s10.histats.com/js15_as.js');document.head.appendChild(hs);})();
+var _Hasync=_Hasync||[];_Hasync.push(['Histats.start','1,5037469,4,0,0,0,00010000']);_Hasync.push(['Histats.fasi','1']);_Hasync.push(['Histats.track_hits','']);(function(){var hs=document.createElement('script');hs.type='text/javascript';hs.async=true;hs.src=('//s10.histats.com/js15_as.js');document.head.appendChild(hs);})();
 
 function getCurrentRoute() {
   let path = window.location.pathname.replace(/\/+$/, '') || '/';
